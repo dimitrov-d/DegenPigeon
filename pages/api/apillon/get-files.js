d087a1a0006b843e45e84a1aa81e9df4a7da42a0
Damjan Dimitrov <damjandimitrov1@gmail.com>
2024-12-11 10:36:39 +0100

    Fix get-files load time

--- pages/api/apillon/get-files.js
+++ pages/api/apillon/get-files.js
@@ -35,18 +35,10 @@ export default async function handler(req, res) {
     }
 
     try {
-      const allBucketFilesAndDirectories = await bucket.listObjects({
+      const data = await bucket.listObjects({
         limit: 10000,
+        search: directoryPath.trim()
       });
-      const directoryUuid = allBucketFilesAndDirectories.items.find(
-        (item) => item.type === 1 && item.name === directoryPath
-      )?.uuid;
-
-      if (!directoryUuid) {
-        return res.status(404).json({ error: 'Directory not found' });
-      }
-
-      const data = await bucket.listObjects({ directoryUuid, limit: 1000 });
 
       res.status(200).json({ data });
     } catch (error) {
